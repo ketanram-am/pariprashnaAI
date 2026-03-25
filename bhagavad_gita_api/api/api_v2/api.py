06549deb277c3af1438f379515e84128cd810e41
Ketan Ram <ketanramamara123@gmail.com>
2026-03-25 12:10:45 +0530

    Skip social routes when optional deps are missing

--- bhagavad_gita_api/api/api_v2/api.py
+++ bhagavad_gita_api/api/api_v2/api.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
-from bhagavad_gita_api.api.api_v2.endpoints import gita, social
+from bhagavad_gita_api.api.api_v2.endpoints import gita
+
+try:
+    from bhagavad_gita_api.api.api_v2.endpoints import social
+except Exception:
+    social = None
 
 api_router = APIRouter()
 api_router.include_router(gita.router)
-api_router.include_router(social.router, include_in_schema=True)
+if social is not None:
+    api_router.include_router(social.router, include_in_schema=True)
